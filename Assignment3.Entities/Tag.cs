f34ab51f1f4a86ddc93e625a22119bce1a112708
Adrian Kari <adrka@itu.dk>
2022-09-23 13:36:08 +0200

    Implemented Entities

--- Assignment3.Entities/Tag.cs
+++ Assignment3.Entities/Tag.cs
@@ -2,4 +2,7 @@ namespace Assignment3.Entities;
 
 public class Tag
 {
+    int Id { get; set; }
+    string Name { get; set; }
+    public virtual ICollection<Task>? Tasks { get; set; }
 }
--- Assignment3.Entities/Task.cs
+++ Assignment3.Entities/Task.cs
@@ -2,4 +2,10 @@ namespace Assignment3.Entities;
 
 public class Task
 {
+    public int Id { get; set; }
+    public string? Title { get; set; }
+    public User? AssignedTo { get; set; }
+    public string? Description { get; set; }
+    public virtual ICollection<Tag>? Tags { get; set; }
+    
 }
