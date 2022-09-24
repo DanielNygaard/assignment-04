e641526798efa6011b59702e110d163b3d77362d
Adam Hadou Temsamani <ahad@itu.dk>
2022-09-24 16:45:24 +0200

    kanban

--- Assignment3.Entities/KanbanContext.cs
+++ Assignment3.Entities/KanbanContext.cs
@@ -1,5 +1,21 @@
+using Microsoft.EntityFrameworkCore;
+using Assignment3;
+
 namespace Assignment3.Entities;
 
-public class KanbanContext
+public class KanbanContext : DbContext
 {
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Task>(entity =>
+        {
+            entity.Property(e => e.Title).HasMaxLength(100);
+            entity.Property(e => e.Title).IsRequired();
+
+            entity.Property(e => e.State).HasConversion(v => v.ToString(),v => (EnumState)Enum.Parse(typeof(EnumState),v));
+
+        });
+    }
+
+
 }
--- Assignment3.Entities/Task.cs
+++ Assignment3.Entities/Task.cs
@@ -6,6 +6,7 @@ public class Task
     public string? Title { get; set; }
     public User? AssignedTo { get; set; }
     public string? Description { get; set; }
+    public enum State {New, Active, Resolved, Closed, Removed}
     public virtual ICollection<Tag>? Tags { get; set; }
     
 }
