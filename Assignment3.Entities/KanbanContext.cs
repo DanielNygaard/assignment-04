4a3bfeb615286cccadeff799299db9474e0f70ac
Adam Hadou Temsamani <ahad@itu.dk>
2022-09-24 17:18:28 +0200

    Worked on exercise 3

--- Assignment3.Entities/KanbanContext.cs
+++ Assignment3.Entities/KanbanContext.cs
@@ -12,10 +12,32 @@ public class KanbanContext : DbContext
             entity.Property(e => e.Title).HasMaxLength(100);
             entity.Property(e => e.Title).IsRequired();
 
+            entity.Property(e => e.Description).HasMaxLength(max);
+            entity.Property(entity => e.Description).IsRequired(false);
+
             entity.Property(e => e.State).HasConversion(v => v.ToString(),v => (EnumState)Enum.Parse(typeof(EnumState),v));
+            
+
+        });
+
+        modelBuilder.Entity<User>(entity =>
+        {  
+            entity.Property(e => e.Name).HasMaxLength(100);
+            entity.Property(e => e.Name).IsRequired();
+
+            entity.Property(e => e.Email).HasMaxLength(100);
+            entity.Property(e => e.Email).IsRequired();
+            entity.Property(e => e.Email).IsUnique();
+
+        });
+
+        modelBuilder.Entity<Tag>(entity =>
+        {
+            entity.Property(e => e.Name).HasMaxLength(50);
+            entity.Property(e => e.Name).IsRequired();
+            entity.Property(e => e.Name).IsUnique();
 
         });
     }
-
 
 }
