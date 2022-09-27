3c790505965efe6e571d1bcdae86505a9887bcda
Adam Hadou Temsamani <ahad@itu.dk>
2022-09-27 16:00:06 +0200

    Fixed ModelBuilder + Many-to-Many constraint

--- Assignment3.Entities/KanbanContext.cs
+++ Assignment3.Entities/KanbanContext.cs
@@ -1,8 +1,9 @@
-using Microsoft.EntityFrameworkCore;
+
 using Assignment3;
 using System;
 using System.Collections.Generic;
 using System.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace Assignment3.Entities;
 
@@ -42,15 +43,9 @@ public class KanbanContext : DbContext
 
         });
 
-        modelBuilder.Entity<WorkItem>().HasKey(e => new {e.Tag});
-        modelBuilder.Entity<WorkItem>().HasOne(e => e.Tag)
-                                       .WithMany(w => w.WorkItem)
-                                       .HasForeignKey(e => e.Tag);
-        
-        modelBuilder.Entity<Tag>().HasKey(e => new {e.WorkItem});
-        modelBuilder.Entity<Tag>().HasOne(e => e.WorkItem)
-                                  .WithMany(t => t.Tag)
-                                  .HasForeignKey(e => e.WorkItem);
+        modelBuilder.Entity<WorkItem>()
+            .HasMany<Tag>(wi => wi.Tags)
+            .WithMany(t => t.WorkItems);
 
     }
 
--- Assignment3.Entities/User.cs
+++ Assignment3.Entities/User.cs
@@ -1,4 +1,4 @@
-using Microsoft.EntityFrameworkCore;
+
 
 namespace Assignment3.Entities;
 
@@ -7,5 +7,5 @@ public class User
     public int Id { get; set; }
     public string? Name { get; set; }
     public string? Email { get; set; }
-    public List<WorkItem>? WorkItems { get; set; }
+    public ICollection<WorkItem>? WorkItems { get; set; }
 }
